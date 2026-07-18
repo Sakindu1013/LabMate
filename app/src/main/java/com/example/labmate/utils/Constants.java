7d7e10fa0b521cb03250868ddb51ae1182cb33c8
Sakindu Dinsara <dinsarasakindu@gmail.com>
2026-07-19 00:24:49 +0530

    Class for roles

---
+++ app/src/main/java/com/example/labmate/utils/Constants.java
@@ -0,0 +1,12 @@
+package com.example.labmate.utils;
+
+public class Constants {
+    public static final String PREF_NAME = "UserPrefs";
+    public static final String KEY_ROLE = "role";
+
+    public static final String ROLE_ADMIN = "Admin";
+    public static final String ROLE_ACADEMIC = "Academic Staff";
+    public static final String ROLE_NON_ACADEMIC = "Non-academic Staff";
+    public static final String ROLE_EXTERNAL = "Postgraduate Student";
+    public static final String ROLE_UNDERGRAD = "Undergraduate Student";
+}
